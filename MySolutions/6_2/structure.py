d5cc490486a077bfab7ef0984ce072530d11c9f0
Carlton Brady <cbrady@ycharts.com>
2024-06-21 14:28:02 -0400

    exercise 6.2

---
+++ MySolutions/6_2/structure.py
@@ -0,0 +1,30 @@
+import sys
+
+
+class Structure:
+    _fields: tuple[str]
+
+    @staticmethod
+    def _init():
+        locs = sys._getframe(1).f_locals
+        self = locs.pop('self')
+        for name, val in locs.items():
+            setattr(self, name, val)
+
+    def __setattr__(self, field, val):
+        if field not in self._fields and not field.startswith('_'):
+            raise AttributeError(f'No attribute {field}')
+
+        super().__setattr__(field, val)
+
+
+    def __repr__(self):
+        properties = []
+        for field in self._fields:
+            val = getattr(self, field)
+            if isinstance(val, str):
+                properties.append(f"'{val}'")
+            else:
+                properties.append(str(val))
+        return f'{type(self).__name__}({", ".join(properties)})'
+
