665a23f77466a396a23071b9a514a5f93b6cd008
Carlton Brady <ctombrady@gmail.com>
2024-07-14 23:17:05 -0400

    exercise 7.6

---
+++ MySolutions/7_6/stock.py
@@ -0,0 +1,22 @@
+from structure import Structure
+
+
+class Stock(Structure):
+    name = String()
+    shares = PositiveInteger()
+    price = PositiveFloat()
+
+    def __eq__(self, other_stock):
+        return (
+                self.name == other_stock.name and 
+                self.shares == other_stock.shares and 
+                self.price == other_stock.price
+        )
+
+    @property
+    def cost(self):
+        return self.shares * self.price
+
+    def sell(self, n: PositiveInteger):
+        self.shares -= n
+
